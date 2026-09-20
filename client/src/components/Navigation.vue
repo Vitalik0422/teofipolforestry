65aeb25d034ea458002ee7c26c69442d5e97ea49
Віталій <vitaliy19980422@gmail.com>
2026-09-20 17:57:44 +0300

    fix: correct navigation item path and remove unnecessary semicolons in Navigation.vue

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
 withDefaults(
   defineProps<{
-    variant?: 'primary' | 'surface';
+    variant?: 'primary' | 'surface'
   }>(),
   {
     variant: 'primary',
   },
-);
-import AppButton from './UI/AppButton.vue';
+)
+import AppButton from './UI/AppButton.vue'
 
 const navigationItems = [
   { to: '/', text: 'Головна' },
   { to: '/about', text: 'Про підприємство' },
-  { to: '/map', text: 'Карта рубок' },
+  { to: '/forestMap', text: 'Карта рубок' },
   { to: '/sales', text: 'Купити дрова' },
   { to: '/news', text: 'Новини' },
   { to: '/contacts', text: 'Контакти' },
-];
+]
 </script>
 
 <template>
