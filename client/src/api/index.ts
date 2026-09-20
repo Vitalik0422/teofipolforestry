80330fb9a47ff923f50600210052afa636e1b41b
Віталій <vitaliy19980422@gmail.com>
2026-09-20 13:52:50 +0300

    fix: pack json

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'http://192.168.0.102:3001',
+  baseURL: import.meta.env.BACKEND_URL || 'http://192.168.0.102:3001',
   withCredentials: true,
 })
